69b71d9dba9374fd816debe9774a9844a8ef0837
Bracaglia <shaws0598@gmail.com>
2022-09-29 15:03:42 -0700

    initial commit

---
+++ src/com/caresoft/clinicapp/User.java
@@ -0,0 +1,14 @@
+package com.caresoft.clinicapp;
+
+public class User {
+    protected Integer id;
+    protected int pin;
+
+    // TO DO: Getters and setters
+
+    User(Integer id){
+        this.id = id;
+    }
+
+    // Implement a constructor that takes an ID
+}
